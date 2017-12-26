cd6c8f407d010f43f688a64c5f5dd034315644dc
Arseny Amerbaev <amerbaev@gmail.com>
2017-12-26 09:45:24 +0300

    meeeeh

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-SAMPLE_SIZE = 500
+SAMPLE_SIZE = 100
 TEST_SIZE = 1
 
 if __name__ == '__main__':
     sunspot_df = pd.read_csv('zuerich-monthly-sunspot-numbers-.csv')
     sunspot_df.drop(sunspot_df.index[[-1]], inplace=True)
-    values = sunspot_df['Zuerich monthly sunspot numbers 1749-1983']
+    Y = sunspot_df['count']
+    # print(X, Y)
     sns.set()
-    plt.plot(values.index, values.values)
+    sunspot_df.plot(x=['year', 'month'], y='count')
+    test = []
+    pred = []
 
-    for i in range(len(values) - SAMPLE_SIZE):
-        sample = values[i:i+SAMPLE_SIZE]
-        x_train, x_test = np.array(sample[:SAMPLE_SIZE-TEST_SIZE].index).reshape(-1, 1), np.array(sample[SAMPLE_SIZE-TEST_SIZE:].index).reshape(-1, 1)
-        y_train, y_test = sample[:-TEST_SIZE].values, sample[-TEST_SIZE:].values
+    for i in range(len(Y) - SAMPLE_SIZE):
+        sample = sunspot_df[i:i+SAMPLE_SIZE]
+
+        x_train, x_test = sample[['year', 'month']][:-TEST_SIZE], sample[['year', 'month']][-TEST_SIZE:]
+        y_train, y_test = sample['count'][:-TEST_SIZE], sample['count'][-TEST_SIZE:]
 
         lin_regr = LinearRegression()
         lin_regr.fit(x_train, y_train)
         lin_y_pred = lin_regr.predict(x_test)
-        plt.scatter(x_test, lin_y_pred, color='red')
+        test.append(y_test)
+        pred.append(lin_y_pred)
+        plt.plot(x_test, lin_y_pred, color='red')
 
+    # print('Среднеквадратичное отклонение: ', mean_squared_error(test, pred))
+    # print('Дисперсия:', r2_score(test, pred))
     plt.show()
